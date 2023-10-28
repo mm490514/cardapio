69d2e4fa1f6acd4599fc6e34a00dc22800f9e11b
MatheusMendes <50601403+mm490514@users.noreply.github.com>
2023-10-28 11:34:01 -0300

    update bd

--- sql/cardapio.sql
+++ sql/cardapio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 04:23 PM
+-- Generation Time: Oct 28, 2023 at 04:33 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -59,9 +59,7 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nome`, `imagem`, `status`) VALUES
-(1, 'Bebidas', '', 1),
-(2, 'Combos', '', 1),
-(3, 'Lanches', '', 1);
+(29, 'Diversos', '', 1);
 
 -- --------------------------------------------------------
 
@@ -108,10 +106,17 @@ CREATE TABLE `pedidosvendas` (
 --
 
 INSERT INTO `pedidosvendas` (`id`, `item_id`, `name`, `price`, `quantity`, `cliente_nome`, `observacao`, `order_date`, `order_id`, `num_mesa`, `status`) VALUES
-(30, 36, 'X tudão', 50, 1, 'Martin', 'X tudão sem ovo', '2023-09-26', 530701, 1, 0),
-(31, 9, 'Combo Clássico', 30, 1, 'Martin', 'X tudão sem ovo', '2023-09-26', 530701, 1, 0),
-(32, 10, 'Combo Salada', 25, 1, 'Martin', 'X tudão sem ovo', '2023-09-26', 530701, 1, 0),
-(33, 8, 'Fanta', 8, 1, 'Martin', 'X tudão sem ovo', '2023-09-26', 530701, 1, 0);
+(77, 36, 'X tudão', 50, 1, 'MATHEUS HENRIQUE DE OLIVEIRA MENDES', 'Sem ovo', '2023-10-11', 531021, 1, 2),
+(78, 9, 'Combo Clássico', 30, 1, 'MATHEUS HENRIQUE DE OLIVEIRA MENDES', 'Hamburguer Duplo', '2023-10-11', 531021, 1, 2),
+(79, 10, 'Combo Salada', 25, 2, 'Matheus', 'Coca', '2023-10-11', 390957, 2, 3),
+(80, 10, 'Combo Salada', 25, 2, 'Matheus', 'Coca', '2023-10-11', 390957, 2, 1),
+(81, 36, 'X tudão', 50, 2, 'Matheus', '1 Sem Ovo', '2023-10-11', 727447, 3, 1),
+(82, 9, 'Combo Clássico', 30, 1, 'Matheus', 'Fanta', '2023-10-11', 727447, 3, 1),
+(83, 8, 'Fanta', 8, 1, 'Rosa', '', '2023-10-28', 862789, 1, 2),
+(84, 9, 'Combo Clássico', 30, 2, 'Rosa', '', '2023-10-28', 862789, 1, 2),
+(85, 36, 'X tudão', 50, 1, 'Rosa', '', '2023-10-28', 788027, 1, 2),
+(86, 8, 'Fanta', 8, 1, 'Rosa', '', '2023-10-28', 647177, 1, 2),
+(87, 36, 'X tudão', 50, 1, 'Teste', 'Teste', '2023-10-28', 863544, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -128,16 +133,6 @@ CREATE TABLE `produtos` (
   `images` text NOT NULL,
   `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `produtos`
---
-
-INSERT INTO `produtos` (`id`, `name`, `id_categoria`, `price`, `description`, `images`, `status`) VALUES
-(8, 'Fanta', 1, 8, '', 'fanta.jpg', 1),
-(9, 'Combo Clássico', 2, 30, '', 'combo-classico.jpg', 1),
-(10, 'Combo Salada', 2, 25, '', 'combo-salada.jpg', 1),
-(36, 'X tudão', 3, 50, 'vem tudo', 'xtudo.png', 1);
 
 --
 -- Indexes for dumped tables
@@ -165,15 +160,13 @@ ALTER TABLE `forma_pgto`
 -- Indexes for table `pedidosvendas`
 --
 ALTER TABLE `pedidosvendas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `item_id` (`item_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `produtos`
 --
 ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_categoria` (`id_categoria`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -189,7 +182,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `forma_pgto`
@@ -201,29 +194,13 @@ ALTER TABLE `forma_pgto`
 -- AUTO_INCREMENT for table `pedidosvendas`
 --
 ALTER TABLE `pedidosvendas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT for table `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `pedidosvendas`
---
-ALTER TABLE `pedidosvendas`
-  ADD CONSTRAINT `pedidosvendas_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `produtos` (`id`);
-
---
--- Constraints for table `produtos`
---
-ALTER TABLE `produtos`
-  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
