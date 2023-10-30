4eeba03692ea56d4bad95a7ec55af31df1326c7a
MatheusMendes <50601403+mm490514@users.noreply.github.com>
2023-10-29 21:18:38 -0300

    Logo

--- admin/login.php
+++ admin/login.php
@@ -38,17 +38,12 @@ if (!empty($_POST["login"]) && !empty($_POST["email"]) && !empty($_POST["passwor
     <div class="container h-100">
         <div class="row justify-content-sm-center h-100">
             <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-                <div class="text-center my-5">
-                    <h1 class="fs-4 card-title fw-bold mb-4">Admin</h1>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg>
-                    <!-- <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100"> -->
+                <div class="text-center my-5">                                                            
                 </div>
                 <div class="card shadow-lg">
+                <img src="./../images/logo.png" class="img-fluid">		   
                     <?php if ($loginMessage != '') { ?>
-                        <div id="login-alert" class="alert alert-primary col-sm-12"><?php echo $loginMessage; ?></div>
+                       
                     <?php } ?>
 
 
@@ -66,7 +61,7 @@ if (!empty($_POST["login"]) && !empty($_POST["email"]) && !empty($_POST["passwor
 
                         <div class="form-group">
                             <div style="text-align: center; margin-bottom: 10px;">
-                                <input type="submit" name="login" value="Login" class="btn btn-primary ms-auto">
+                                <input type="submit" name="login" value="Login" class="btn btn-danger ms-auto">
                             </div>
                         </div>
                     </form>
--- inc/header.php
+++ inc/header.php
@@ -13,3 +13,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="/css/alert.css">
+    <link rel="icon" href="http://127.0.0.1:8046/cardapio/images/icon.png" type="image/png">
